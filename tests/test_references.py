9013e3dec84190aed9dc6df1fdf5448a7b3bd906
Ben Webb <bjwebb67@googlemail.com>
2018-09-15 17:46:31 +0900

    Add test of JSON refs when opening a file

--- tests/test_references.py
+++ tests/test_references.py
@@ -4,6 +4,7 @@ import sys
 from collections import OrderedDict
 import json
 from sphinxcontrib.jsonschema import JSONSchema
+import tempfile
 
 if sys.version_info < (2, 7):
     import unittest2 as unittest
@@ -23,6 +24,35 @@ class TestJSONReference(unittest.TestCase):
                 "a": {"$ref": "#/definitions/thing"}
             }
         }""").attributes
+        expected = json.loads("""{
+            "definitions": {
+                "thing": {
+                    "title": "stuff"
+                }
+
+            },
+            "properties": {
+                "a": {
+                    "title": "stuff"
+                }
+            }
+        }""", object_pairs_hook=OrderedDict)
+        self.assertEqual(resolved, expected)
+
+    def test_jsonschema_load(self):
+        with tempfile.TemporaryFile(mode='w+') as fp:
+            fp.write("""{
+                "definitions": {
+                    "thing": {
+                        "title": "stuff"
+                    }
+                },
+                "properties": {
+                    "a": {"$ref": "#/definitions/thing"}
+                }
+            }""")
+            fp.seek(0)
+            resolved = JSONSchema.load(fp).attributes
         expected = json.loads("""{
             "definitions": {
                 "thing": {
